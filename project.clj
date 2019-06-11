09aff1465b511ffb63d8cba3a85ffafca5cd649b
Tej <vishnutej@gmail.com>
2019-06-11 13:35:59 -0400

    Update project.clj

--- project.clj
+++ project.clj
@@ -5,4 +5,5 @@
             :url "http://www.eclipse.org/legal/epl-v10.html"}
   :dependencies [[org.clojure/clojure "1.8.0"]
                  [clojurewerkz/neocons "3.1.0"]
-                 ])
+                 ]
+  :main import-to-neo.core/insert-dimensions)
